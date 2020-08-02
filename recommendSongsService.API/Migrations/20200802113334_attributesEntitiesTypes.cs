aa6785a6296b73a4dfb5275d0fdca1378c00910e
Claudia Gomes <crejane10@gmail.com>
2020-08-02 09:04:32 -0700

    migration to correcting erros in entities

---
+++ recommendSongsService.API/Migrations/20200802113334_attributesEntitiesTypes.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace recommendSongsService.Migrations
+{
+    public partial class attributesEntitiesTypes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Users",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Users",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Email",
+                table: "Users",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.InsertData(
+                table: "Users",
+                columns: new[] { "Id", "Email", "Hometown", "Name", "Password" },
+                values: new object[,]
+                {
+                    { 1, 0, null, "CROG", null },
+                    { 2, 0, null, "Docker Magazine", null }
+                });
+        }
+    }
+}
